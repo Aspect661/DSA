ebb44be5dc5ad3cd0e393476bda83dadb221e99c
Tuhin Das <145273953+Aspect661@users.noreply.github.com>
2026-05-26 19:25:12 -0700

    Time: 3 ms (99.01%) | Memory: 20.1 MB (36.86%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,8 +1,7 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         
-        dup_s = "".join([char for char in s if char.isalnum()])
-        dup_s = dup_s.lower()
+        dup_s = ("".join([char for char in s if char.isalnum()])).lower()
         if dup_s == dup_s[::-1]:
             return True
         else: 
