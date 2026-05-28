555faefb420cb8a29ce51104fa003b9902d170bd
Tuhin Das <145273953+Aspect661@users.noreply.github.com>
2026-05-28 00:54:20 -0700

    Time: 1 ms (51.18%) | Memory: 19.4 MB (22.97%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -11,17 +11,27 @@ class Solution:
             
             return sum
         
-        store_num = []
-        store_num.append(square(n))
+        # store_num = []
+        # store_num.append(square(n))
 
-        while 1 not in store_num:
-            num = square(store_num[-1])
-            if num == 1:
-                return True
-            elif num in store_num: 
-                return False
-            else:
-                store_num.append(num)
+        # while 1 not in store_num:
+        #     num = square(store_num[-1])
+        #     if num == 1:
+        #         return True
+        #     elif num in store_num: 
+        #         return False
+        #     else:
+        #         store_num.append(num)
         
-        return square(n) == 1
+        # return square(n) == 1
             
+        slow = square(n)
+        fast = square(square(n))
+
+        while(slow!=fast):
+            if slow == 1 or fast == 1:
+                return True
+            slow = square(slow)
+            fast = square(square(fast))
+        
+        return slow == 1
